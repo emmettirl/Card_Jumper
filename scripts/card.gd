aed1fc28dc1f1302f615f80024bda58cc11c775b
Emmett <emmettirl@gmail.com>
2024-03-14 20:45:25 +0000

    Test init value,

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var NameLabel = $Panel/MarginContainer/VBoxContainer/MarginContainer/NameLabel
-@onready var ImageBG = $Panel/MarginContainer/VBoxContainer/MarginContainer2/ImageBG
+@onready var CardImage = $Panel/MarginContainer/VBoxContainer/MarginContainer2/ImageBG/MarginContainer/TextureRect
 @onready var Direction = $Panel/MarginContainer/VBoxContainer/MarginContainer3/HBoxContainer/Direction
 @onready var Magnitude = $Panel/MarginContainer/VBoxContainer/MarginContainer4/HBoxContainer2/Magnitude
 @onready var CardText = $Panel/MarginContainer/VBoxContainer/MarginContainer5/CardTextLabel
@@ -9,9 +9,12 @@ extends Control
 var vector
 
 func initialize_card(direction, magnitude):
-    Direction.text = direction
-    Magnitude.text = magnitude
-    
+    Direction.text = str(direction)
+    Magnitude.text = str(magnitude)
+
     vector = Vector2(float(direction), float(magnitude))
-    
-    # todo programatically decide image, name and text based on vector
+
+func _ready():
+    initialize_card(1,2)
+        
+# todo programatically decide image, name and text based on vector
