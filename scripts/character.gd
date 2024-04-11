596907f3bdb6b288d4a695f080da2359e307dcb8
Emmett <emmettirl@gmail.com>
2024-04-11 19:54:55 +0100

    Set up character animations

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var collision = $CollisionShape2D
+@onready var Sprite = $Sprite2D
 
 const DAMPING = 0.975
 
@@ -22,9 +23,27 @@ func _physics_process(delta):
     position.x = clamp(position.x, 0 + collision.shape.extents.x , get_viewport().get_visible_rect().size.x -collision.shape.extents.x)
     
     var coll = move_and_collide(velocity * delta)
-    if not is_on_floor() and coll:
+    if not is_on_floor() and coll and abs(velocity.y) > 0.001:
         velocity = velocity.bounce(coll.get_normal())
 
     if velocity.y < 0:
         velocity.y *= DAMPING
     velocity.x *= DAMPING
+    
+    
+
+    if velocity.y > 300:
+        if velocity.x <0:
+            Sprite.animation = "down_left"
+        else:
+            Sprite.animation = "down_right"
+    elif velocity.y < -300:
+        if velocity.x <0:
+            Sprite.animation = "up_left"
+        else:
+            Sprite.animation = "up_right"
+    else:
+        if velocity.x <0:
+            Sprite.animation = "default_left"
+        else:
+            Sprite.animation = "default"
