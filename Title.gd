ff388daa8c0dc049efd3510e1cf7cfdfad80c9e4
Emmett <emmettirl@gmail.com>
2024-05-07 21:05:20 +0100

    Cleaned up some unneeded references which were causing errors

--- Title.gd
+++ Title.gd
@@ -9,7 +9,7 @@ func _ready():
     print("Title Screen Ready")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
     pass
     
 func _on_button_pressed():
